40a91f82eed6f6bfa3a02064ef945333abab7df5
Travis Mix <travismix1980@gmail.com>
2021-02-24 01:12:04 -0800

    Getting our data from wikipedia but its in an html format

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,28 +1,56 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Search = () => {
-	const [term, setTerm] = useState('');
-	const [results, setResults] = useState([]);
+  const [term, setTerm] = useState("programming");
+  const [results, setResults] = useState([]);
 
-	useEffect(() => {
-		console.log('Search Ran!(at some point');
+  useEffect(() => {
+		const search = async () => {
+			const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+				params: {
+					action: "query",
+					list: "search",
+					origin: "*",
+					format: "json",
+					srsearch: term,
+				},
+			});
+			setResults(data.query.search);
+		}
+    search();
 	}, [term]);
-
-	return (
-		<div>
-			<div className="ui form">
-				<div className="field">
-					<label>Enter Search Term</label>
-					<input
-						className="input"
-						value={term}
-						onChange={e => setTerm(e.target.value)}
-					/>
+	
+	const renderedResults = results.map((result) => {
+		return (
+			<div key={result.pageid} className="item">
+				<div className="content">
+					<div className="header">
+						{result.title}
+					</div>
+					{result.snippet}
 				</div>
 			</div>
-		</div>
-	);
-	
+		);
+	});
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term</label>
+          <input
+            className="input"
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+          />
+        </div>
+			</div>
+			<div className="ui celled list">
+				{renderedResults}
+			</div>
+    </div>
+  );
 };
 
 export default Search;
